36189a61befe96e0553bd3b340694d7f6900ff04
ducky-fish <35616959+ducky-fish@users.noreply.github.com>
2018-11-13 16:57:41 -0700

    Update Challenge1.java

--- Challenge1.java
+++ Challenge1.java
@@ -6,22 +6,22 @@
 */
 
 public class ASum {
-	public static long findNb(long m) {
-  	long blocks = 0;      //we must return a long and compare longs
-    long volume = 0;
+     public static long findNb(long m) {
+	long blocks = 0;      //we must return a long and compare longs
+    	long volume = 0;
 //The while loop below is adding one block to blocks every loop
 //The loop invariant for this is: volume < m 
 //because this must be held before and after the loop 
-  while(volume < m){      
-    blocks++;
-    volume += blocks*blocks*blocks;
+  	while(volume < m){      
+    		blocks++;
+    		volume += blocks*blocks*blocks;
 //The if checks if we have reached our goal while in the loop
-    if(volume == m){
-      return blocks;
-    }
-  }
+    	if(volume == m){
+      		return blocks;
+    		}
+  	}
 //If we find that we haven't reached our goal of volume == m
 //then volume > m and we should return -1 to indicate no fitting blocks
   return -1;
-}
+	}
 }
