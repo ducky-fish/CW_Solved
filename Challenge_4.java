9aaf42e5416a078bca37641ffcf1e0a50f99502c
ducky-fish <35616959+ducky-fish@users.noreply.github.com>
2018-11-30 11:48:50 -0700

    Create Challenge_4.java

---
+++ Challenge_4.java
@@ -0,0 +1,26 @@
+//Incomplete Below
+public class Main {
+	public static void main(String[] args){
+		tester();
+	}
+
+	public static void tester(){
+	int Test_Case = 9149;
+  	System.out.print(squareDigits(Test_Case));
+	}
+/* Square Every Digit
+ * We will square every digit of a number
+ * we receive an int and need to return 
+ * an int. e.g. 9119 --> 811181
+ * because 9^2 = 81 and 1^2 = 1
+ */
+public static int squareDigits(int n) {
+  if(n<10){
+    return n * n;
+  }
+  if(){
+
+  }
+  }
+
+}
